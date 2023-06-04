851b9ae8bcd494edd1141a67badc77f41681cfea
LuisGonzalez558 <a1120250002@utch.edu.mx>
2023-06-04 00:38:14 -0600

    justfinalsection

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  expanded = false; // Variable para controlar el estado de la sección
+  buttonText = 'Load More'; // Texto inicial del botón
 
+  toggleSection() {
+    this.expanded = !this.expanded;
+
+    if (this.expanded) {
+      this.buttonText = 'Mostrar menos';
+    } else {
+      this.buttonText = 'Load More';
+    }
+  }
+  selectedPart: string = 'part1';
+
+  changeContent(part: string) {
+    this.selectedPart = part;
+  }
 }
