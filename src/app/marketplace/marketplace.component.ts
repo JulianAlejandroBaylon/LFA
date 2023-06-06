58e473e3a7ad61d0665746f4e13d0cda4173cf7f
JulianAlejandroBaylon <superhazzard2@gmail.com>
2023-06-06 12:14:39 -0600

    Ultimos detalles

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -5,4 +5,52 @@ import { Component } from '@angular/core';
   templateUrl: './marketplace.component.html',
   styleUrls: ['./marketplace.component.css'],
 })
-export class MarketplaceComponent {}
+export class MarketplaceComponent {
+  showAll: boolean = true;
+  showLegendary: boolean = false;
+  showRare: boolean = false;
+  showFanatic: boolean = false;
+  showNormal: boolean = false;
+
+  // ...
+
+  all() {
+    this.showAll = true;
+    this.showLegendary = false;
+    this.showRare = false;
+    this.showFanatic = false;
+    this.showNormal = false;
+  }
+
+  legendary() {
+    this.showAll = false;
+    this.showLegendary = true;
+    this.showRare = false;
+    this.showFanatic = false;
+    this.showNormal = false;
+  }
+
+  rare() {
+    this.showAll = false;
+    this.showLegendary = false;
+    this.showRare = true;
+    this.showFanatic = false;
+    this.showNormal = false;
+  }
+
+  fanatic() {
+    this.showAll = false;
+    this.showLegendary = false;
+    this.showRare = false;
+    this.showFanatic = true;
+    this.showNormal = false;
+  }
+
+  normal() {
+    this.showAll = false;
+    this.showLegendary = false;
+    this.showRare = false;
+    this.showFanatic = false;
+    this.showNormal = true;
+  }
+}
