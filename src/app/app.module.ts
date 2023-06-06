1180b3c642b6168d20f467543f1946f2d5e71254
JulianAlejandroBaylon <superhazzard2@gmail.com>
2023-06-06 00:44:45 -0600

    Navbar cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
-import { AppRoutingModule } from './app-routing.module';
+
 import { LoginComponent } from './login/login.component';
 import { SignComponent } from './sign/sign.component';
 import { FooterComponent } from './footer/footer.component';
@@ -14,6 +14,8 @@ import { ChallengesComponent } from './challenges/challenges.component';
 
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Ruta por defecto
+  { path: 'home', component: HomeComponent },
   { path: 'marketplace', component: MarketplaceComponent },
   { path: 'challenges', component: ChallengesComponent },
   { path: 'login', component: LoginComponent },
@@ -31,7 +33,7 @@ const routes: Routes = [
     FooterComponent,
     ChallengesComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  constructor(private router: Router) { }
+
+  navigateToHome() {
+    this.router.navigate(['home']);
+  }
+
+  navigateToMarketplace() {
+    this.router.navigate(['/marketplace']);
+  }
+
+  navigateToChallenges() {
+    this.router.navigate(['/challenges']);
+  }
+
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
